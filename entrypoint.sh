ea1a8374a43b594121d63eea6c3f48e15d38f83f
buntel <julianbantel@gmail.com>
2020-08-11 11:58:24 +0200

    support multiple files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,19 @@
 # $2 :: ${{ inputs.auth_user }}
 # $3 :: ${{ inputs.auth_email }}
 # $4 :: ${{ inputs.gist_url }}
-# $5 :: ${{ inputs.file }}
-# $6 :: ${{ inputs.history }}
+# $5 :: ${{ inputs.dir }}
+# $6 :: ${{ inputs.files }}
+# $7 :: ${{ inputs.history }}
 
 auth_token=$1
 auth_user=$2
 auth_email=$4
 gist_url=$4
-file=$5
-history=$6
+dir=$5
+files=$6
+history=$7
+
+DIR=$(pwd)
 
 git config --global user.email ${auth_email}
 git config --global user.name ${auth_user}
@@ -20,14 +24,17 @@ git config --global user.name ${auth_user}
 gist_url_array=($(sed 's|//|\n|g' <<< "$gist_url"))
 
 git clone ${gist_url_array[0]}//${auth_user}:${auth_token}@${gist_url_array[1]} gist
-file_name_parts=($(echo $file | tr "/" "\n"))
-gist_file=${file_name_parts[-1]}
-if [[ ! -f gist/${gist_file} ]]; then
-    touch gist/${gist_file}
-fi
-cat ${file} > gist/${gist_file}
-cd gist
-git add ${gist_file}
+for file in ${files}; do
+    cd $DIR
+    echo ${dir}/${file}
+    gist_file=gist/${file}
+    if [[ ! -f ${gist_file} ]]; then
+        touch ${gist_file}
+    fi
+    cat ${dir}/${file} > ${gist_file}
+    cd gist
+    git add ${file}
+done
 if [[ "$history" = true ]]; then
     git commit -m 'update gist via gist-sync-action'
     git push
